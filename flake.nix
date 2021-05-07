6224d4ccd38ac1afd5f41d5ed82713213c6f15fb
Euan Kemp <euank@euank.com>
2021-05-07 00:58:05 -0700

    Add protobuf dependency

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  description = "nixek api devshell";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = nixpkgs.legacyPackages.${system}; in
+    {
+      devShell = import ./shell.nix { inherit pkgs; };
+    });
+}
